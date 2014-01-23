f79bddd2e6b6a47d47f112f5cecb1abbe96d3463
Filip <filip.dbrsk@gmail.com>
2014-01-23 15:42:53 +0100

    View

---
+++ view.py
@@ -0,0 +1,31 @@
+'''
+Filip Ilievski
+Emanuele Crosato
+Michael Trouw
+'''
+
+execfile('controller.py')
+
+def print_bag(bag):
+	s=""
+	for item in b.vegetables:
+		s+=item.name + ","
+	s+=b.fruit.name
+	print s
+
+bag_type=raw_input("\nDear customer, \nWelcome to the f00d KBS. \n\nPlease select the type of bag (enter 1 or 2):\n1 - Normal bag\n2 - Christmas special bag\n")
+
+
+bags=create_configurations(bag_type)
+
+print "Final set of bags:\n"
+
+count=1
+for b in bags:
+	s=str(count) + " - "
+	print s
+	print_bag(b)
+	count+=1
+
+chosen_bag=raw_input("\nPlease select your bag (enter 1-5):")
+print "\nYou chose " + chosen_bag # TODO: Instead of this, insert the new bag in the DB!!!
