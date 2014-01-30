f621dc0c4b5a1c2fbc372ea37b080d47afbff5cb
Emanuele Crosato <Mag00tz@feweb58066.feweb.vu.nl>
2014-01-30 16:21:29 +0100

    comments emanuele

--- classes.py
+++ classes.py
@@ -4,12 +4,14 @@ Emanuele Crosato
 Michael Trouw
 '''
 
+''' This file defines all the classes of the project''' 
+
 class Bag(object):
 
     def __init__(self):
-        self.price = 0
-        self.fruit = ''
-        self.vegetables = []
+        self.price = 0          # total price of the bag
+        self.fruit = ''         # the only fruit
+        self.vegetables = []    # the list of the 4 vegetables
 
     def __str__(self):
         return "i am a bag, yay. my price is: " % (self.price, self.num_items)
@@ -18,19 +20,19 @@ class Bag(object):
 class OrganicThing(object):
 
     def __init__(self):
-        self.name = ''
-        self.category = ''
-        self.price = 0
-        self.pieces = 0
-        self.seasons = ['']
-        self.origin = ''
-        self.easyToPrepare = ''
-        self.color = ''
-        self.perishability = ''
-        self.oftenUsed = ''
-        self.forgotten = ''
-        self.recipes = []
-        self.xmasRecipes = []
+        self.name = ''              # name of the item
+        self.category = ''          # fruit or vegetable
+        self.price = 0              # price of the item
+        self.pieces = 0             # number of pieces it is composed (ex. 1 for watermelon and 4 for apple)
+        self.seasons = ['']         # season in which the item grows
+        self.origin = ''            # regional, national, european or worldwide
+        self.easyToPrepare = ''     # easy of difficult to prepare
+        self.color = ''             # color of the item
+        self.perishability = ''     # slow, normal or fast
+        self.oftenUsed = ''         # whether the item is used to cook many things, such as onions and garlic
+        self.forgotten = ''         # whether the item is forgotten or not
+        self.recipes = []           # list of normal recipes names in which the item appears
+        self.xmasRecipes = []       # list of Christmas recipes names in which the items appears
 
     def __str__(self):
         return "%s: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s." % (self.category, self.name, self.price, self.pieces, \
@@ -40,6 +42,6 @@ class OrganicThing(object):
 class Recipe(object):
 
     def __init__(self):
-        self.name = ''
-	self.description = ''
-        self.ingredients = []
+        self.name = ''              # name of the recipe
+        self.description = ''       # description
+        self.ingredients = []       # list of ingredients
--- databaseInterface.py
+++ databaseInterface.py
@@ -1,61 +1,78 @@
-import xmltodict,json,io
+'''
+Filip Ilievski
+Emanuele Crosato
+Michael Trouw
+'''
+
+''' This file implements the interface of the database, providing all the functions to interact with it''' 
+
+import xmltodict,json,io 	# in case xmltodict is not installed: "sudo pip install xmltodict"
 
 from classes import OrganicThing, Bag, Recipe
 
+############################# INTERNAL FUNCTINOS ###############################
+
+# returns the entire database as a Python dictionary
 def objectifyWholeDB(database_path):
-	fileX = io.open(database_path, 'r')
-	o = xmltodict.parse(fileX.read())
-	my_obj = eval(json.dumps(o))
+	fileX = io.open(database_path, 'r') # open the database xml file
+	o = xmltodict.parse(fileX.read()) 	# parse the xml
+	my_obj = eval(json.dumps(o)) 		# translate from tree to dict
 	return my_obj
 
+# add to an item all the recipes in which it appears 
 def addRecipes(item):
-	recipes = get_all_recipes()
-	for r in recipes:
-		if item.name in r.ingredients:
-			item.recipes.append(r.name)
-	recipes = get_all_xmas_recipes()
+	recipes = get_all_recipes()					# get all recipes from the database
+	for r in recipes:							# for each recipe
+		if item.name in r.ingredients:			#	if it contains the item
+			item.recipes.append(r.name)			# 		the recipe is added to the item's list of recipes
+	recipes = get_all_xmas_recipes()			# same for the Christmas recipes
 	for r in recipes:
 		if item.name in r.ingredients:
 			item.xmasRecipes.append(r.name)
 
+# creates an object of type OrganicThing from a simple dictionary representing a vegetable
 def create_vegetable(v):
 	new_vegetable = OrganicThing()
-        new_vegetable.name = v['name']
-        new_vegetable.category = 'vegetable'
-        new_vegetable.price = float(v['price'])
-        new_vegetable.pieces = v['pieces']
-        seasons = []
-        for s in v['seasons']['season']:
-        	seasons.append(s)
-        new_vegetable.seasons = seasons
-        new_vegetable.origin = v['origin']
-        new_vegetable.easyToPrepare = v['easyToPrepare']  == 'yes'
-        new_vegetable.color = v['color']
-        new_vegetable.perishability = v['perishability']
-        new_vegetable.oftenUsed = v['oftenUsed']  == 'yes'
-        new_vegetable.forgotten = v['forgotten']  == 'yes'
-        addRecipes(new_vegetable)
+	new_vegetable.name = v['name']
+	new_vegetable.category = 'vegetable'
+	new_vegetable.price = float(v['price'])		# the price read from the database as a string,
+												# so it has to be converted
+	new_vegetable.pieces = v['pieces']
+	seasons = []
+	for s in v['seasons']['season']:
+		seasons.append(s)
+	new_vegetable.seasons = seasons
+	new_vegetable.origin = v['origin']
+	new_vegetable.easyToPrepare = v['easyToPrepare']  == 'yes' # string to boolean
+	new_vegetable.color = v['color']
+	new_vegetable.perishability = v['perishability']
+	new_vegetable.oftenUsed = v['oftenUsed']  == 'yes' 			# string to boolean
+	new_vegetable.forgotten = v['forgotten']  == 'yes' 			# string to boolean
+	addRecipes(new_vegetable)	# call the function for adding all the recipes to the item
 	return new_vegetable
 
+# creates an object of type OrganicThing from a simple dictionary representing a fruit 
 def create_fruit(f):
 	new_fruit = OrganicThing()
 	new_fruit.name = f['name']
 	new_fruit.category = 'fruit'
-	new_fruit.price = float(f['price'])
+	new_fruit.price = float(f['price'])			# the price read from the database as a string,
+												# so it has to be converted
 	new_fruit.pieces = f['pieces']
 	seasons = []
 	for s in f['seasons']['season']:
 		seasons.append(s)
 	new_fruit.seasons = seasons
 	new_fruit.origin = f['origin']
-	new_fruit.easyToPrepare = f['easyToPrepare'] == 'yes'
+	new_fruit.easyToPrepare = f['easyToPrepare'] == 'yes'	# string to boolean
 	new_fruit.color = f['color']
 	new_fruit.perishability = f['perishability']
-	new_fruit.oftenUsed = None
-	new_fruit.forgotten = None
-	addRecipes(new_fruit)
+	new_fruit.oftenUsed = None	# fruits have no value for this field
+	new_fruit.forgotten = None	# fruits have no value for this field
+	addRecipes(new_fruit)	# call the function for adding all the recipes to the item
 	return new_fruit
 
+# serach for a fruit in the database given its name. Returns it as a dict
 def get_fruit_by_name(strFruit):
 	whole = objectifyWholeDB('database.xml')
 	fruits = whole['foodDatabase']['food']['fruits']['fruit']
@@ -64,6 +81,7 @@ def get_fruit_by_name(strFruit):
 			return f
 	return None
 
+# serach for a vegetable in the database given its name. Returns it as a dict
 def get_vegetable_by_name(strVegetable):
 	whole = objectifyWholeDB('database.xml')
 	vegs = whole['foodDatabase']['food']['vegetables']['vegetable']
@@ -72,79 +90,7 @@ def get_vegetable_by_name(strVegetable):
 			return v
 	return None
 
-############################################################
-
-def get_all_fruits():
-	whole = objectifyWholeDB('database.xml')
-	fruitsDict = whole['foodDatabase']['food']['fruits']['fruit']
-	fruits = []
-	for f in fruitsDict:
-		new_fruit=create_fruit(f)
-		fruits.append(new_fruit)
-	return fruits
-
-def get_all_vegetables():
-	whole = objectifyWholeDB('database.xml')
-	vegetablesDict = whole['foodDatabase']['food']['vegetables']['vegetable']
-	vegetables = []
-	for v in vegetablesDict:
-		new_vegetable=create_vegetable(v)
-		vegetables.append(new_vegetable)
-	return vegetables
-
-def get_previous_bag():
-	whole = objectifyWholeDB('database.xml')
-	bagDict = whole['foodDatabase']['baghistory']['bag'][0]
-	bag = Bag()
-	bag.price = float(bagDict['price'])
-
-	strFruit = bagDict['foodItems']['fruitItem']
-	completeFruit = get_fruit_by_name(strFruit)
-	bag.fruit =create_fruit(completeFruit)
-
-	vegetables = []
-	for v in bagDict['foodItems']['vegetableItem']:
-		completeVegetable=get_vegetable_by_name(v)
-		vegetable=create_vegetable(completeVegetable)
-		vegetables.append(vegetable)
-	bag.vegetables = vegetables
-	return bag
-
-def get_all_recipes():
-	whole = objectifyWholeDB('database.xml')
-	repicesDict = whole['foodDatabase']['recipes']['recipe']
-	recipes = []
-	for r in repicesDict:
-		new_recipe = Recipe()
-		new_recipe.name = r['name']
-		new_recipe.description = r['description']
-		for ingredient in r['ingredient']:
-			new_recipe.ingredients.append(ingredient)
-		recipes.append(new_recipe)
-	return recipes
-
-def get_all_xmas_recipes():
-	whole = objectifyWholeDB('database.xml')
-	repicesDict = whole['foodDatabase']['xMasRecipes']['recipe']
-	recipes = []
-	for r in repicesDict:
-		new_recipe = Recipe()
-		new_recipe.name = r['name']
-		new_recipe.description = r['description']
-		for ingredient in r['ingredient']:
-			new_recipe.ingredients.append(ingredient)
-		recipes.append(new_recipe)
-	return recipes
-
-def addNewBagIntoDatabase(bag):
-	modifiedBag = prepareBagForConversion(bag)
-	newDatabaseDict = objectifyWholeDB('database.xml')
-	newDatabaseDict['foodDatabase']['baghistory']['bag'].insert(0, modifiedBag)
-	newDatabaseXML = xmltodict.unparse(newDatabaseDict)
-	databaseFile = open("database.xml", "w")
-	databaseFile.write(newDatabaseXML)
-	databaseFile.close()
-
+# translates a Bag into a dict
 def prepareBagForConversion(bag):
 	modifiedBag = {}
 	modifiedBag['foodItems'] = {}
@@ -155,3 +101,86 @@ def prepareBagForConversion(bag):
 	modifiedBag['foodItems']['vegetableItem'] = vegetables
 	modifiedBag['price'] = bag.price
 	return modifiedBag
+
+
+############################# FUNCTINOS THAT ARE CALLED FROM EXTERNAL MODULES ###############################
+
+# reads all fruits from database and returns them as a list or OrganicThing
+def get_all_fruits():
+	whole = objectifyWholeDB('database.xml')	# get the whole database as a Python dict
+	fruitsDict = whole['foodDatabase']['food']['fruits']['fruit'] # select only the fruits
+	fruits = []
+	for f in fruitsDict:			# for each fruit
+		new_fruit=create_fruit(f)	#	create the relative OrganicThing
+		fruits.append(new_fruit)	#	append it to the list
+	return fruits
+
+# reads all vegetables from database and returns them as a list or OrganicThing
+def get_all_vegetables():
+	whole = objectifyWholeDB('database.xml')	# get the whole database as a Python dict
+	vegetablesDict = whole['foodDatabase']['food']['vegetables']['vegetable'] # select only the vegetables
+	vegetables = []
+	for v in vegetablesDict:				# for each vegetable
+		new_vegetable=create_vegetable(v)	#	create the relative OrganicThing
+		vegetables.append(new_vegetable)	#	append it to the list
+	return vegetables
+
+# reads the most recent bag from database and returns it as a Bag
+def get_previous_bag():
+	whole = objectifyWholeDB('database.xml')	# get the whole database as a Python dict
+	bagDict = whole['foodDatabase']['baghistory']['bag'][0] # selects only the last bag (it is always the most recent)
+	bag = Bag()
+	bag.price = float(bagDict['price'])			# the price read from the database as a string,
+												# so it has to be converted
+	strFruit = bagDict['foodItems']['fruitItem']
+	completeFruit = get_fruit_by_name(strFruit)	# the bag contains only the name of the fruit but the entire entity
+												# is here needed
+	bag.fruit =create_fruit(completeFruit)
+	vegetables = []
+	for v in bagDict['foodItems']['vegetableItem']:
+		completeVegetable=get_vegetable_by_name(v)		# the bag contains only the name of the vegetables but the entire
+														# entities are here needed
+		vegetable=create_vegetable(completeVegetable)
+		vegetables.append(vegetable)
+	bag.vegetables = vegetables
+	return bag
+
+# reads all normal recipes from database and returns them as a list of Recipe
+def get_all_recipes():
+	whole = objectifyWholeDB('database.xml')	# get the whole database as a Python dict
+	repicesDict = whole['foodDatabase']['recipes']['recipe'] # selects only the normal recipes
+	recipes = []
+	for r in repicesDict:
+		new_recipe = Recipe()
+		new_recipe.name = r['name']
+		new_recipe.description = r['description']
+		for ingredient in r['ingredient']:
+			new_recipe.ingredients.append(ingredient)
+		recipes.append(new_recipe)
+	return recipes
+
+# reads all Christmas recipes from database and returns them as a list of Recipe
+def get_all_xmas_recipes():
+	whole = objectifyWholeDB('database.xml')	# get the whole database as a Python dict
+	repicesDict = whole['foodDatabase']['xMasRecipes']['recipe'] # selects only the Christmas recipes
+	recipes = []
+	for r in repicesDict:
+		new_recipe = Recipe()
+		new_recipe.name = r['name']
+		new_recipe.description = r['description']
+		for ingredient in r['ingredient']:
+			new_recipe.ingredients.append(ingredient)
+		recipes.append(new_recipe)
+	return recipes
+
+# inserts a new bag into the database as first bag item
+def addNewBagIntoDatabase(bag):
+	modifiedBag = prepareBagForConversion(bag)		# the bag is given in input as an Object and it has to be
+													# translated into a dict in order to be translated into XML
+													# using the library xmltodict
+	newDatabaseDict = objectifyWholeDB('database.xml') 	# read database
+	newDatabaseDict['foodDatabase']['baghistory']['bag'].insert(0, modifiedBag) # insert as first
+	newDatabaseXML = xmltodict.unparse(newDatabaseDict)
+	databaseFile = open("database.xml", "w") 			# rewrite updated database
+	databaseFile.write(newDatabaseXML)
+	databaseFile.close()
